0729d5c47b5ac978604a01258e652f320aaa1f53
Gustavo99Roa <gustavoarauj509@gmail.com>
2025-07-20 17:02:06 -0300

    finalizado operadores

---
+++ Clase 1/Clase 2/operadores.py
@@ -0,0 +1,22 @@
+"""a==b
+a!=b
+a>b
+a<b
+a>=b
+a<=b
+"""
+#operaradores de comparacion
+
+a=10
+b=18
+r=a==b
+r= a!=b
+print(r)
+r=a>b
+print("Es menor", r)
+r=a<b
+print("Es mayor", r)
+r=a <=b
+print(r)
+r=a>=b
+print(r)

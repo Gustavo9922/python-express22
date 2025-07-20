66aa346592c9bce4c3433e230f40ab6699ed8574
Gustavo99Roa <gustavoarauj509@gmail.com>
2025-07-20 17:18:50 -0300

    operaciones aritmeticos

---
+++ Clase 1/Clase 2/aritmeticos.py
@@ -0,0 +1,27 @@
+"""
+Operadores aritmeticos basicos en Python
+sumas, restas, multiplicaciones y divisiones dos numeros
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a//b
+h=a%b
+"""
+a=17
+b=18
+r=0
+r=a+b
+print("suma", r)
+r=a-b
+print("resta", r)
+r=a*b
+print("multiplicacion", r)
+r=a/b
+print("division", r)
+r=a%b
+print("modulo", r)
+r=a**b
+print("exponente", r)
+# Operadores aritmeticos
+

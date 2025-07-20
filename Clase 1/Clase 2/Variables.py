321305ae2915fa55725032e669b18bb4685b480e
Gustavo99Roa <gustavoarauj509@gmail.com>
2025-07-20 16:45:46 -0300

    finalizados variables

---
+++ Clase 1/Clase 2/Variables.py
@@ -0,0 +1,39 @@
+"""
+Esto es un comentario de varias lineas
+puedes escribir lo que quieras
+Incididunt nisi labore aute ipsum aliquip amet fugiat minim tempor aliquip culpa do. Eiusmod duis anim veniam anim exercitation cupidatat exercitation adipisicing duis tempor consequat consectetur commodo ullamco. Sint esse quis cupidatat voluptate officia esse nulla sit cupidatat occaecat enim consectetur anim quis. Enim commodo veniam mollit mollit. Veniam excepteur est laboris eu est dolore dolore laboris proident.
+
+Sit est aliqua nostrud velit occaecat. Nisi incididunt labore adipisicing cillum aliqua enim labore veniam. Nisi reprehenderit eu do esse est occaecat voluptate. Magna ipsum eu ea proident sit sit aliquip reprehenderit aute dolor duis id. Cillum velit laboris irure sit minim consequat dolore laborum dolor veniam. Laboris excepteur nulla adipisicing consequat nulla do velit. Aliquip fugiat ut commodo Lorem est voluptate adipisicing reprehenderit in officia.
+
+Et dolor eu non irure eiusmod culpa dolor quis veniam elit sunt elit. Voluptate irure magna commodo exercitation excepteur laborum. Ex incididunt elit pariatur ullamco Lorem elit veniam ipsum ut do elit culpa fugiat. Lorem exercitation eiusmod cupidatat magna cillum id adipisicing fugiat consequat. Laborum incididunt pariatur in amet.
+
+Aliqua commodo pariatur dolore nisi reprehenderit pariatur. Mollit occaecat aute cillum Lorem non sit deserunt mollit cupidatat laboris reprehenderit dolore deserunt. Reprehenderit elit irure eu magna ut nulla aliquip ut eu aliquip.
+
+Adipisicing qui culpa sint proident ipsum est occaecat est tempor eiusmod duis. Consequat nostrud tempor anim et exercitation id laborum qui ipsum adipisicing. Ex minim voluptate laboris esse reprehenderit ad id ullamco ipsum eiusmod commodo proident eu. Duis tempor Lorem aliquip non."""
+
+
+
+print("hola mundo desde la unida")
+
+entero=10
+flotante=10.5
+cadena="hola mundo"
+booleano=True
+print(entero,flotante,cadena,booleano)
+print(type(entero),type(flotante),type(cadena),type(booleano))
+entero=10.5
+print(type(entero))
+
+print("suma dos numeros")
+# Operaciones con variables
+# suma, resta, multiplicacion y division
+a=10
+b=20
+print("suma de dos numeros", a+b)
+print("restando dos numeros")
+print("resta de dos numeros", a-b)
+print("multiplicando dos numeros")
+print("multiplicacion de dos numeros", a*b)
+b=2
+print("dividiendo dos numeros")
+print("division de dos numeros", a/b)
